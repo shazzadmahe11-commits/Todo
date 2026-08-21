88ba65d08455b12a1467415318ab6612ad182c1d
shazzadmahe11-commits <shazzadmahe11@gmail.com>
2026-08-21 02:14:43 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <meta name="theme-color" content="#f5faf6" />
+        <meta name="theme-color" content="#0e1013" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="apple-mobile-web-app-title" content="Kamla.com" />
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       </head>
